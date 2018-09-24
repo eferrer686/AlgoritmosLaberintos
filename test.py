3e1ea88dce4736aae98d4cc8cbe1a788ef4436a5
eferrer686 <edfmgr@hotmail.com>
2018-09-24 13:04:57 -0500

    Comentarios de test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import time
+start_time = time.time()
+
 import fileinput
 size = []
 begin = []
@@ -8,12 +11,16 @@ cont = 0
 
 for line in fileinput.input():
     if cont==0:
+        #Leer Tamaño de laberinto
         size = line
     elif cont==1:
+        #Leer coordenada de inicio
         begin = line
     elif cont==2:
+        #Leer coordenada de final
         finish = line
     if cont>2:
+        #Leer laberinto quitando el caracter "Enter"
         maze.append(line.replace("\n",''))
     cont += 1
 
@@ -22,3 +29,11 @@ print(begin)
 print(finish)
 print(maze)
 
+#Voltear laberinto sobre el eje x
+maze.reverse()
+print(maze)
+
+
+elapsed_time = time.time() - start_time
+print("Time:")
+print(elapsed_time)
